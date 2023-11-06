c9efd2ceab0e0644f3aa6806ee7baaad781421b3
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-06 22:08:56 +0000

    added homepage formatting

--- dice.rb
+++ dice.rb
@@ -4,7 +4,14 @@ require "better_errors"
 require "binding_of_caller"
 
 get ("/") do
-  "Hello, world!"
+  "<h1>Dice Roll</h1>
+  
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided dice</a></li>
+    <li><a href=\"/dice/5/4\"Roll five 4-sided dice</a></li>
+  </ul>"
 end
 
 get ("/dice/2/6") do
